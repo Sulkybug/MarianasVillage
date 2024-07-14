acef2e6486b90cf736fe70c79645e5d13680a201
Sulkybug <feang_c0@hotmail.com>
2024-07-14 01:37:04 -0500

    form input radio and styles form

--- src/Pages/Dating/DatingPage.tsx
+++ src/Pages/Dating/DatingPage.tsx
@@ -4,12 +4,12 @@ import Footer from "../../Components/Footer/Footer";
 import LateralBar from "../../Components/LateralBar/LateralBar";
 import "./DatingPage.css";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { FaPaperPlane } from "react-icons/fa";
 
 type Inputs = {
   name: string;
   email: string;
   Subject: string;
+  gender: string;
   message: string;
 };
 
@@ -58,70 +58,111 @@ const DatingPage: React.FC = () => {
     <div>
       <Navbar onScroll={onScroll} />
       <div id="dating">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {errors.name && <div className="text-red">{errors.name.message}</div>}
-          <input
-            {...register("name", {
-              required: "Name is required",
-              minLength: {
-                value: 3,
-                message: "Name must have at least 3 characters",
-              },
-            })}
-            type="text"
-            id="name"
-            placeholder="What's your name?"
-          />
-          {errors.email && (
-            <div className="text-red">{errors.email.message}</div>
-          )}
-          <input
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                message: "Invalid email address",
-              },
-            })}
-            type="email"
-            id="email"
-            placeholder="Email address"
-          />
-          {errors.Subject && (
-            <div className="text-red">{errors.Subject.message}</div>
-          )}
-          <input
-            {...register("Subject", { required: "A subject is required" })}
-            type="text"
-            id="Subject"
-            placeholder="Subject"
-          />
-          {errors.message && (
-            <div className="text-red">{errors.message.message}</div>
-          )}
-          <textarea
-            {...register("message", {
-              required: "Write a message please",
-              minLength: {
-                value: 8,
-                message: "Message is too short to be sent",
-              },
-            })}
-            id="message"
-            cols={80}
-            rows={6}
-            placeholder="Type your message here"
-          ></textarea>
-          <button
-            type="submit"
-            className="sendMessage"
-            disabled={loading || isSubmitting}
-          >
-            {loading || isSubmitting ? "Loading..." : "Send Message"}
-            &thinsp;
-            <FaPaperPlane />
-          </button>
-        </form>
+        <div className="formBox">
+          <h1 className="titleformDating">Find Love in Colombia</h1>
+          <hr />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {errors.name && (
+              <div className="text-red">{errors.name.message}</div>
+            )}
+            <input
+              {...register("name", {
+                required: "Name is required",
+                minLength: {
+                  value: 3,
+                  message: "Name must have at least 3 characters",
+                },
+              })}
+              type="text"
+              id="name"
+              placeholder="What's your name?"
+            />
+            {errors.email && (
+              <div className="text-red">{errors.email.message}</div>
+            )}
+            <input
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "Invalid email address",
+                },
+              })}
+              type="email"
+              id="email"
+              placeholder="Email address"
+            />
+            {errors.Subject && (
+              <div className="text-red">{errors.Subject.message}</div>
+            )}
+            <input
+              {...register("Subject", { required: "A subject is required" })}
+              type="text"
+              id="Subject"
+              placeholder="Subject"
+            />
+            {errors.gender && (
+              <div className="text-red">{errors.gender.message}</div>
+            )}
+            <legend>Gender</legend>
+            <div className="options">
+              <div>
+                <input
+                  {...register("gender", { required: "Select an option" })}
+                  type="radio"
+                  id="male"
+                  value="male"
+                  name="gender"
+                />
+                <label htmlFor="male">Male</label>
+              </div>
+              <div>
+                <input
+                  {...register("gender", { required: "Select an option" })}
+                  type="radio"
+                  id="female"
+                  value="female"
+                  name="gender"
+                />
+                <label htmlFor="female">Female</label>
+              </div>
+              <div>
+                <input
+                  {...register("gender", { required: "Select an option" })}
+                  type="radio"
+                  id="noAnswer"
+                  value="noAnswer"
+                  name="gender"
+                />
+                <label htmlFor="noAnswer">Rather not say</label>
+              </div>
+            </div>
+            {errors.message && (
+              <div className="text-red">{errors.message.message}</div>
+            )}
+            <textarea
+              {...register("message", {
+                required: "Write a message please",
+                minLength: {
+                  value: 8,
+                  message: "Message is too short to be sent",
+                },
+              })}
+              id="message"
+              cols={80}
+              rows={6}
+              placeholder="Type your message here"
+            ></textarea>
+            <button
+              type="submit"
+              className="sendMessage"
+              disabled={loading || isSubmitting}
+            >
+              {loading || isSubmitting ? "Loading..." : "Send Message"}
+              &thinsp;
+            </button>
+          </form>
+        </div>
       </div>
       <LateralBar />
       <Footer />
